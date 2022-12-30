ba3af56d3e3060ceea04a991b00be5958732149e
lukinvadim101 <71687637+lukinvadim101@users.noreply.github.com>
2022-12-30 14:03:31 -0600

    admin check re

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :admin?, only: %i[create update destroy edit new]
-
-  def admin?
-    redirect_to root_path, notice: 'You need Admin rights' if current_user&.has_role?(:admin)
+  before_action only: %i[new edit create update destroy] do
+    redirect_to root_path, notice: 'You need Admin rights' if current_user && !current_user.admin?
   end
 end
