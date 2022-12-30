27aa7d824adcc291a38597b6276d4510af7596aa
lukinvadim101 <71687637+lukinvadim101@users.noreply.github.com>
2022-12-30 13:41:36 -0600

    user role check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :is_admin?, only: %i[create update destroy edit new], except: [index]
+  before_action :admin?, only: %i[create update destroy edit new], except: [index]
 
-  def is_admin?
+  def admin?
     redirect_to root_path, notice: 'You need Admin rights' if current_user.has_role? :admin
   end
 end
